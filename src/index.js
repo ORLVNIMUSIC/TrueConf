7fd9b0618f094ec406dc498e9bae7948e08968db
ORLVNIMUSIC <orlvnimusic@gmail.com>
2022-08-17 18:39:48 +0300

    clean up, testing, adding dist folder

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var round_1 = require("./figures/round");
-var rectangle_1 = require("./figures/rectangle");
-var myRound = new round_1.Round(2);
-console.log(myRound.area());
-var myRect = new rectangle_1.Rectangle(4, 3);
-console.log(myRect.area());
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Round } from './figures/round';
 import { Rectangle } from './figures/rectangle';
+import { Triange } from './figures/triangle';
 
 const myRound: Round = new Round(2);
 console.log(myRound.area());
 
 const myRect: Rectangle = new Rectangle(4, 3);
 console.log(myRect.area());
+
+const myTriangle: Triange = new Triange(2, 3);
+console.log(myTriangle.area());
