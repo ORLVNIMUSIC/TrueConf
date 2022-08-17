ae8356fbaeedac317887e22b3a430fa0c542924b
ORLVNIMUSIC <orlvnimusic@gmail.com>
2022-08-17 19:30:43 +0300

    added tsdoc for figure

--- src/figures/figure.ts
+++ src/figures/figure.ts
@@ -1,4 +1,16 @@
+/**
+ * Standart interface for all figures
+ *
+ * All figurs must implement all methods of the interface
+ */
 export default interface Figure {
+  /**
+   * method for calculating area of the object
+   */
   area(): number;
+
+  /**
+   * method for calculating perimeter of the object
+   */
   perimeter(): number;
 }
