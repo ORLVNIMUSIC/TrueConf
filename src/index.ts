b1270a0b443e14373cb5a6b46fa33ab182fabd54
ORLVNIMUSIC <orlvnimusic@gmail.com>
2022-08-17 20:07:29 +0300

    export of the library

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import { Round } from './figures/round';
-import { Rectangle } from './figures/rectangle';
-import { Triange } from './figures/triangle';
-
-const myRound: Round = new Round(5);
-console.log(myRound.perimeter());
-
-const myRect: Rectangle = new Rectangle(4, 3);
-console.log(myRect.perimeter());
-
-const myTriangle: Triange = new Triange(7, 3);
-console.log(myTriangle.perimeter());
+export { Round } from './figures/round';
+export { Rectangle } from './figures/rectangle';
+export { Triange } from './figures/triangle';
